87f3b90391b1edba44a3c0d50313db0c32345643
Kyryh <kyrykiller07@gmail.com>
2024-05-08 22:38:10 +0200

    Update Traversable.cs

--- Assets/Scripts/Spaces/Traversable.cs
+++ Assets/Scripts/Spaces/Traversable.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+[DisallowMultipleComponent]
 public abstract class Traversable : MonoBehaviour {
 
     public List<Traversable> nextTraversable = new();
