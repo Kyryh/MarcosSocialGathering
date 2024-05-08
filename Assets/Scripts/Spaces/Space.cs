edb045c2309d3d82030775fd3821bb2ee93cc068
Kyryh <kyrykiller07@gmail.com>
2024-05-08 22:38:35 +0200

    Added space icon renderer

--- Assets/Scripts/Spaces/Space.cs
+++ Assets/Scripts/Spaces/Space.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 public abstract class Space : Traversable
 {
     public Sprite spaceIcon;
+
+    public SpriteRenderer spaceIconRenderer;
     public virtual bool ShouldCountForDiceRolls => true;
     public virtual void OnLanding() {
 
+    }
+
+    public override void Awake() {
+        if (spaceIconRenderer != null) { 
+            spaceIconRenderer.sprite = spaceIcon;
+        }
+    }
+    private void OnValidate() {
+        Awake();
     }
 }
