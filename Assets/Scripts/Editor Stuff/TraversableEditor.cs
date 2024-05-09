42a54c8a01d3c3f7ec0bb7b13e58d2e6aedc1294
Kyryh <kyrykiller07@gmail.com>
2024-05-09 19:27:15 +0200

    Added editor stuff for traversables
    Now it automatically links it together to selected traversables when you spawn one

---
+++ Assets/Scripts/Editor Stuff/TraversableEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class TraversableEditor : MonoBehaviour {
+
+
+    private void Awake() {
+        if (!Application.isPlaying && TryGetComponent<Traversable>(out var traversable)) {
+            foreach(var transform in Selection.transforms) {
+                if (transform.TryGetComponent<Traversable>(out var other)) {
+                    other.nextTraversable.Add(traversable);
+                }
+            }
+        }
+    }
+}
