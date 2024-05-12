228e6bd562ba02f951b89fa2832f12f3134da557
Kyryh <kyrykiller07@gmail.com>
2024-05-12 20:38:11 +0200

    Fixed player managers not being reset after shutdown

--- Assets/Scripts/Managers/NetworkManagerHelper.cs
+++ Assets/Scripts/Managers/NetworkManagerHelper.cs
@@ -48,6 +48,10 @@ public class NetworkManagerHelper : MonoBehaviour
 
     public void Shutdown() {
         NetworkManager.Singleton.Shutdown();
+        foreach (var player in GameManager.Instance.players)
+        {
+            player.Deactivate();
+        }
         SceneManager.LoadScene("MainMenu");
     }
 }
